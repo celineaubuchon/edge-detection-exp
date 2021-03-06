6e54f4650b1a44506e62d5124107480e19f486a4
catherine <catherine@catherines-mbp.devices.brown.edu>
2021-03-06 13:59:54 -0500

    pair coding for 2 hours, wrote the edge detection function

--- edgedetection.m
+++ edgedetection.m
@@ -9,8 +9,11 @@
     
 %% Main script
     % preforms the operations
-    img = imread("images/tserre.jpg");
+    %img = imread("images/tserre.jpg");
+    %convolve(zeros(10,10), img);
+    img = ones(100) * 255/2;
     convolve(zeros(10,10), img);
+    
     
 
 %% Functions
@@ -22,20 +25,34 @@
         % image boundaries? 
     % (?) functions that load in images, and save our output images? Or
     % just put this code in the main script?
-    
+  % pair coded this section for 2 hours  
 function framed = convolve(kernel, photo)
-    photo = im2bw(photo);
+    photo = photo(:,:,2); 
+    % im2bw(photo);
     photoSize = size(photo);
     kernelSize = size(kernel);
     kWidth = floor(kernelSize(2));
+    %imwrite(photo);
+%     framed = zeros(photoSize(1) + kWidth -1, photoSize(2) + kWidth -1);
+%     framed(kWidth/2:end-kWidth/2, kWidth/2:end-kWidth/2) = photo;
+%     disp(framed(kWidth/2 + 1, 20:25));
+%     disp(photo(1,20-kWidth/2:25-kWidth/2));
+%     imwrite(framed, "images/result.jpeg");
+
+    copyPhoto = zeros(photoSize);
     
-    framed = zeros(photoSize(1) + kWidth, photoSize(2) + kWidth);
-    framed(kWidth/2:end-kWidth/2, kWidth/2:end-kWidth/2) = photo;
-    
-    imshow(framed);
-    
-    for col = 1:size(framed, 2)
-        for row = 1:size(framed, 1)
+    for col = 1:size(photo, 2)
+        for row = 1:size(photo, 1)
+            sum = 0;
+            for k = 1:kWidth
+                if ~ (row - kWidth/2 + k -1 < 0) || ~(row - kWidth/2 + k - 1 > photoSize(1)) 
+                    pix = photo(row - kWidth/2 + k -1, col);
+                    weight = kernel(k);
+                    sum = sum + pix*weight;
+                end
+                    
+                    
+                
             % (row, col) is current pixel
 %             sample_code = photo(row : row + size(QR_code, 1)-1, col : col + size(QR_code, 2)-1);
 %             if(isequal(sample_code, QR_code))
@@ -43,6 +60,8 @@ function framed = convolve(kernel, photo)
 %             elseif(isequal(sample_code, rot90(QR_code)))
 %                 detected = true;
 %             end
+            end
+            copyPhoto(row, col) = sum;
         end
     end
 end
@@ -71,4 +90,10 @@ end
 % before applying the sobel operators. (This can be done after we achieve
 % functionality).
 
+% things to do independently:
+% orientation of the edge detection
+% megan - how to do it for color?
+% the statistical tests we want to run and input titles 
+% how to create the grain with computer program
+% demo video??
 
